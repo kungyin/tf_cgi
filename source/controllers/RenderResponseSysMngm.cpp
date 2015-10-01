f6efdb6a7506b9536ef0c442319f10a40fb4b2b9
ian <ian_chen@calcomp.com.tw>
2015-10-01 15:54:30 +0800

    - Fix bug of sorting log.

--- source/controllers/RenderResponseSysMngm.cpp
+++ source/controllers/RenderResponseSysMngm.cpp
@@ -807,7 +807,7 @@ void RenderResponseSysMngm::generateLogSystem() {
 //    QString paraField = m_pReq ->parameter("f_field") ;
 //    QString paraUser = m_pReq ->parameter("user") ;
 
-    QStringList apiOut = getAPIStdOut(API_PATH + SCRIPT_CLOG + " -g 0");
+    QStringList apiOut = getAPIStdOut(API_PATH + SCRIPT_CLOG + " -g 0 | tac");
 
     QVariantMap listMap;
     listMap.insert("total", QString::number(apiOut.size()));
